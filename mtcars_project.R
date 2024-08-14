911ee3e1a42e90ab3b50e0095f79e8cfc42290a5
Wande Folaranmi <wande693@gmail.com>
2024-08-14 18:52:02 +0100

    Add files via upload

---
+++ mtcars_project.R
@@ -0,0 +1,17 @@
+library(datasets)
+
+data(mtcars)
+
+head(mtcars, 5)
+
+library(ggplot2)
+
+ggplot(aes(x=disp, y=mpg), data = mtcars)  
+ggplot(aes(x=disp, y=mpg), data = mtcars) + ggtitle("wande's work") + geom_point() + labs(x= "Displacement (cu.in.)", y= "Miles/(US) gallon")
+
+# covert gear to factor
+mtcars$gear <- as.factor(mtcars$gear)
+ggplot(aes(x=gear, y=mpg), data = mtcars) + geom_boxplot() 
+
+ggplot(aes(x=gear, y=mpg, fill = gear), data = mtcars) + geom_boxplot(alpha = 1)
+theme(legend.position = "none")
